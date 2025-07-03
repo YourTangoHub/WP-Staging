aab49aaf9de35959cfaa0f8e2bcd0de224dade59
Rishabh-aiqmen <rishabh.rawat@aiqmen.ai>
2025-07-03 18:37:28 +0530

    [Custom scss Created & Header UI fixed]

--- wp-content/themes/twentytwentyfive-child/functions.php
+++ wp-content/themes/twentytwentyfive-child/functions.php
@@ -1,61 +1,86 @@
 <?php
-// Enqueue parent and child theme styles
-add_action( 'wp_enqueue_scripts', function() {
-    wp_enqueue_style( 'twentytwentyfive-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'twentytwentyfive-child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( 'twentytwentyfive-style' ),
-        wp_get_theme()->get('Version')
-    );
-});
-
-function add_class_to_first_paragraph($content) {
-    if (is_singular('post') || is_home() || is_archive()) {
-        // Use regex to find the first <p> tag and inject a class
-        $content = preg_replace('/<p(\s+|>)/', '<p class="first-paragraph"$1', $content, 1);
-    }
-    return $content;
-}
-add_filter('the_content', 'add_class_to_first_paragraph');
-
-// function enqueue_open_sans_font() {
-//     wp_enqueue_style(
-//         'open-sans-font',
-//         'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap',
-//         false
-//     );
-// }
-// add_action('wp_enqueue_scripts', 'enqueue_open_sans_font');
-
-function relative_post_time_shortcode() {
-    return sprintf( '%s ago', human_time_diff( get_the_time( 'U' ), current_time( 'timestamp' ) ) );
-}
-add_shortcode( 'relative_time', 'relative_post_time_shortcode' );
-
-
-// Css & JS Remove
+// -----------------------------
+// Selectively remove CSS & JS
+// -----------------------------
 function pm_remove_all_scripts() {
     global $wp_scripts;
-    $wp_scripts->queue = array();
+
+    // Keep only selected script handles if needed
+    $allowed_scripts = array(); // Add script handles here if needed
+
+    foreach ($wp_scripts->queue as $key => $handle) {
+        if (!in_array($handle, $allowed_scripts)) {
+            unset($wp_scripts->queue[$key]);
+        }
+    }
 }
-add_action('wp_print_scripts', 'pm_remove_all_scripts', 100);
+add_action('wp_print_scripts', 'pm_remove_all_scripts', 5); // Run early
+
 function pm_remove_all_styles() {
     global $wp_styles;
-    $wp_styles->queue = array();
-}
-add_action('wp_print_styles', 'pm_remove_all_styles', 100);
-// Css & JS Remove
 
-// emojis
+    // Keep only these style handles
+    $allowed_styles = array(
+        'common-style',
+        'header-style',
+    );
+
+    foreach ($wp_styles->queue as $key => $handle) {
+        if (!in_array($handle, $allowed_styles)) {
+            unset($wp_styles->queue[$key]);
+        }
+    }
+}
+add_action('wp_print_styles', 'pm_remove_all_styles', 5); // Run early
+
+// -----------------------------
+// Enqueue styles
+// -----------------------------
+add_action('wp_enqueue_scripts', function() {
+    wp_enqueue_style(
+        'twentytwentyfive-style',
+        get_template_directory_uri() . '/style.css'
+    );
+
+    wp_enqueue_style(
+        'twentytwentyfive-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('twentytwentyfive-style'),
+        wp_get_theme()->get('Version')
+    );
+
+    wp_enqueue_style(
+        'common-style',
+        get_stylesheet_directory_uri() . '/assets/scss/common.scss',
+        array(),
+        filemtime(get_stylesheet_directory() . '/assets/scss/common.scss')
+    );
+    wp_enqueue_style(
+        'header-style',
+        get_stylesheet_directory_uri() . '/assets/scss/header.scss',
+        array(),
+        filemtime(get_stylesheet_directory() . '/assets/scss/header.scss')
+    );
+}, 1000); // Run late
+
+// -----------------------------
+// Shortcode for "x time ago"
+// -----------------------------
+function relative_post_time_shortcode() {
+    return sprintf('%s ago', human_time_diff(get_the_time('U'), current_time('timestamp')));
+}
+add_shortcode('relative_time', 'relative_post_time_shortcode');
+
+// -----------------------------
+// Disable emoji scripts/styles
+// -----------------------------
 function disable_emojis() {
-   remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-   remove_action( 'wp_print_styles', 'print_emoji_styles' );
-   remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-   remove_action( 'admin_print_styles', 'print_emoji_styles' );
-   remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-   remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-   remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('admin_print_styles', 'print_emoji_styles');
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
 }
-add_action( 'init', 'disable_emojis' );
-// emojis
-
+add_action('init', 'disable_emojis');
